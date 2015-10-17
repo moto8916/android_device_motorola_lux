2220d495cb9ebb10538b1172e9794150738f2595
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-10-16 22:57:53 -0400

    init: add support for Vodafone Australia model
    Change-Id: I6a2466ad6b1a37f3a12c8a7a50dbc5fc4b724447

--- init/init_lux.c
+++ init/init_lux.c
@@ -60,7 +60,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     property_get("ro.boot.hardware.sku", sku);
     property_get("ro.boot.carrier", carrier);
 
-    if (ISMATCH(carrier, "retgb") || ISMATCH(carrier, "reteu")) {
+    if (ISMATCH(carrier, "retgb") || ISMATCH(carrier, "reteu") || ISMATCH(carrier, "vfau")) {
         // These are single SIM XT1562 devices
         property_set("ro.product.model", "XT1562");
         property_set("ro.product.name", "lux_reteu");
