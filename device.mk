997416f1af997e6fa5ac600b13412c83ec37711a
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-10-20 23:01:08 -0400

    Revert "Revert "audio: use prebuilt HAL for now [1/2]""
    This reverts commit 0d4bef27c1f7ae76116f6d9736a97477cb0c4dc1.

--- device.mk
+++ device.mk
@@ -64,7 +64,6 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
-    audio.primary.msm8916 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
@@ -77,7 +76,8 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_florida.xml
 
 # Camera
 PRODUCT_PACKAGES += \
